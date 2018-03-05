56757b8d539d8fd86df97eb65d8244662342b68c
Juan Correa <juancorrea@Juans-MacBook-Pro.local>
2018-03-05 00:11:10 -0800

    Working with friends array

---
+++ app/data/friends.js
@@ -0,0 +1,66 @@
+var friendsArray = [{
+        name: "Giancarlos",
+        photo: "https://scontent-lax3-1.xx.fbcdn.net/v/t31.0-8/22520061_141549113055368_6771266603969379040_o.jpg?oh=d7c32855182d7e4308229fa49796c61d&oe=5B4B2654",
+        scores: [
+            "5",
+            "2",
+            "4",
+            "4",
+            "5",
+            "3",
+            "2",
+            "3",
+            "2",
+            "1"
+        ]
+    },
+    {
+        name: "Eduardo",
+        photo: "https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-1/p320x320/15032180_1859837264244902_338366481957104918_n.jpg?oh=a3207f3b77af8b20c88863275376aa61&oe=5B4ADF72",
+        scores: [
+            "1",
+            "2",
+            "2",
+            "3",
+            "5",
+            "3",
+            "1",
+            "3",
+            "2",
+            "1"
+        ]
+    },
+    {
+        name: "Rob Rosas",
+        photo: "https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-1/p320x320/26731672_10156242522718109_8055003202699986245_n.jpg?oh=0e2ad518ad3e2bed47620bb763a19b4b&oe=5B0251D5",
+        scores: [
+            "5",
+            "3",
+            "1",
+            "3",
+            "5",
+            "3",
+            "3",
+            "3",
+            "2",
+            "1"
+        ]
+    },
+    {
+        name: "Brandon",
+        photo: "https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/27544605_1912638102383207_5959228993026877898_n.jpg?oh=3f0c1a1598d66f7384cbd83af315ce70&oe=5B3CD28E",
+        scores: [
+            "2",
+            "2",
+            "1",
+            "3",
+            "2",
+            "3",
+            "4",
+            "3",
+            "1",
+            "2"
+        ]
+    },
+];
+module.exports = friendsArray;
--- server.js
+++ server.js
@@ -25,6 +25,9 @@ app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
 // parse an HTML body into a string
 app.use(bodyParser.text({ type: 'text/html' }))
 
+require("./app/routing/html-routes.js")(app);
+require("./app/routing/api-routes.js")(app);
+
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
 });
